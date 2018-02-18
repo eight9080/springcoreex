2b38e7727326c1ab44331a1fa445c43301a2a106
eight9080 <eight9080@gmail.com>
2018-02-18 19:59:22 +0200

    autowiring

---
+++ src/main/java/ro/dg/springex/service/autowiring/AutowireByType.java
@@ -0,0 +1,55 @@
+package ro.dg.springex.service.autowiring;
+
+import org.springframework.beans.factory.annotation.Autowire;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class AutowireByType {
+
+    private static class ClientBean {
+        private ServiceBean serviceBean;
+
+        public void setServiceBean (ServiceBean serviceBean) {
+            this.serviceBean = serviceBean;
+        }
+
+        public void doSomething () {
+            System.out.println(serviceBean.getMsg());
+        }
+    }
+
+    private static class ServiceBean {
+        private String msg;
+
+        public ServiceBean (String msg) {
+            this.msg = msg;
+        }
+
+        public String getMsg () {
+            return msg;
+        }
+    }
+
+    @Configuration
+    public static class Config {
+
+        @Bean(autowire = Autowire.BY_TYPE)
+        public ClientBean clientBean () {
+            return new ClientBean();
+        }
+
+        @Bean
+        public ServiceBean serviceBean1 () {
+            return new ServiceBean("Service bean 1");
+        }
+    }
+
+    public static void main (String[] args) {
+        AnnotationConfigApplicationContext context = new
+                AnnotationConfigApplicationContext(Config.class);
+        ClientBean bean = context.getBean(ClientBean.class);
+        bean.doSomething();
+    }
+
+}
---
+++ src/main/java/ro/dg/springex/service/autowiring/AutowireNoExample2.java
@@ -0,0 +1,55 @@
+package ro.dg.springex.service.autowiring;
+
+import org.springframework.beans.factory.annotation.Autowire;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+public class AutowireNoExample2 {
+
+    private static class ClientBean {
+        @Autowired
+        private ServiceBean serviceBean;
+
+        public void doSomething () {
+            System.out.println(serviceBean.getMsg());
+        }
+    }
+
+    private static class ServiceBean {
+        private String msg;
+
+        public ServiceBean (String msg) {
+            this.msg = msg;
+        }
+
+        public String getMsg () {
+            return msg;
+        }
+    }
+
+    public static class Config {
+
+        @Bean(autowire = Autowire.NO)
+        public ClientBean clientBean () {
+            return new ClientBean();
+        }
+
+        @Bean
+        public ServiceBean serviceBean () {
+            return new ServiceBean("Service Bean 1");
+        }
+
+        @Bean
+        public ServiceBean serviceBean2 () {
+            return new ServiceBean("Service Bean 2");
+        }
+    }
+
+    public static void main (String[] args) {
+        AnnotationConfigApplicationContext context = new
+                AnnotationConfigApplicationContext(Config.class);
+        ClientBean bean = context.getBean(ClientBean.class);
+        bean.doSomething();
+    }
+}
